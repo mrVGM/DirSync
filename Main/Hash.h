0b145439d769440966d46bca4e2a69aad69d2455
mrVGM <maddvvas@gmail.com>
2024-02-20 00:45:12 +0200

    hash function

---
+++ Main/Hash.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+namespace crypto
+{
+	typedef struct {
+		unsigned char data[64];
+		unsigned int datalen;
+		unsigned int bitlen[2];
+		unsigned int state[8];
+	} SHA256_CTX;
+
+	std::string HashString(const std::string& data);
+
+	void Init(SHA256_CTX& ctx);
+	void Update(SHA256_CTX& ctx, const unsigned char* data, size_t dataSize);
+	std::string Finalize(SHA256_CTX& ctx);
+}
