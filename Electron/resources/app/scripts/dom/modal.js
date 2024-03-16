ea8955fee9bd415f3d01cf2f47a34963bfb363bc
mrVGM <maddvvas@gmail.com>
2024-03-16 21:16:05 +0200

    modal selector

---
+++ Electron/resources/app/scripts/dom/modal.js
@@ -0,0 +1,43 @@
+let _panel;
+
+const { render } = require('./renderHTML');
+
+function getPanel() {
+    if (!_panel) {
+        _panel = render('modal');
+        document.getElementById('main').appendChild(_panel.element);
+    }
+    
+    return _panel;
+}
+
+function setVisible(visible) {
+    const panel = getPanel();
+    panel.element.style.display = visible ? 'block' : 'none';
+}
+
+async function choose(options) {
+    const panel = getPanel();
+
+    setVisible(true);
+
+    panel.tagged.options_space.innerHTML = '';
+
+    const chosen = await new Promise((resolve, reject) => {
+        for (let i = 0; i < options.length; ++i) {
+            const cur = options[i];
+            const item = render('option');
+            item.tagged.name.innerHTML = cur.name;
+            item.tagged.option.addEventListener('click', () => {
+                resolve(cur);
+            });
+            panel.tagged.options_space.appendChild(item.element);
+        }
+    });
+
+    setVisible(false);
+
+    return chosen;
+}
+
+exports.choose = choose;
